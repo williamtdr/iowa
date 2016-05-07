a93d8329ac3183c31d93a75af2cf1712a0195322
William Teder <wteder@hydreon.com>
2016-05-07 14:02:48 -0500

    Expandable champion tiles

--- static/js/page/summoner.js
+++ static/js/page/summoner.js
@@ -2,8 +2,58 @@
  * Loaded directly on summoner profile pages.
  */
 
+function closeChampionTile(el) {
+	el.data("champion-tile-open", false);
+	el.css({
+		width: "",
+		height: "",
+		"-webkit-filter": ""
+	});
+	el.find(".champion-footer").css({
+		height: ""
+	});
+	el.find(".champion-footer img").css({
+		width: ""
+	});
+	el.find(".champion-footer .champion-name").css({
+		left: "",
+		"font-size": ""
+	});
+}
+
+function openChampionTile(el) {
+	el.data("champion-tile-open", true);
+	el.css({
+		width: "100%",
+		height: "400px",
+		"-webkit-filter": "brightness(100%)"
+	});
+	el.find(".champion-footer").css({
+		height: "96px"
+	});
+	el.find(".champion-footer img").css({
+		width: "96px"
+	});
+	el.find(".champion-footer .champion-name").css({
+		left: "101px",
+		"font-size": "2em"
+	});
+}
+
+function onChampionTileClick(ev) {
+	var el = $(this);
+	if(el.data("champion-tile-open")) {
+		closeChampionTile(el);
+		return false;
+	}
+
+	openChampionTile(el);
+}
+
+
 function loadSummonerInfo(region, id) {
 	$.get("/data/" + region + "/" + id, function(data) {
 		$("#sub-content").html(data);
-	})
+		$(".champion").unbind().click(onChampionTileClick);
+	});
 }
